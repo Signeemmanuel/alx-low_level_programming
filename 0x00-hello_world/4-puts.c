6cf1aef660b885f6f1fe43574e6932140c9b2ea3
Signeemmanuel <signeemmanuel28@gmail.com>
2022-09-11 03:38:58 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.io>
 
+/**
+ *main - Entry point
+ *Return: Always 0
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
 
