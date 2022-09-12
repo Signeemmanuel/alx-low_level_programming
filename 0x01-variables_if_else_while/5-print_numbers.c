1d2377cfc84956ed09387feb94ef006367feffd0
Signeemmanuel <signeemmanuel28@gmail.com>
2022-09-12 16:03:00 -0700

    Add 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ *main: entry point
+ *return: 0
+ * */
+
+int main(void)
+{
+	char ch;
+	for(ch = '0'; ch <= '9'; ch++)
+		printf("%c", c)
+	printf("\n");
+	return (0);
+}
+
+
