b9737d8b023e80096e6efa909f745d5520792568
Signeemmanuel <signeemmanuel28@gmail.com>
2022-12-10 18:18:00 -0800

    Update 3-print_alpahbets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
-/*
- *main: entry point to the program
- *return: 0
+/**
+ * main - Entry point into the program
+ *
+ * Return: Always return 0 after successful execution of the program
  */
-
 int main(void)
 {
 	char ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 	for (ch = 'A'; ch <= 'Z'; ch++)
-		 putchar(ch);
+		putchar(ch);
 	putchar('\n');
 	return (0);
 }
