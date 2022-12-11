8558a333f13e2a8f648e22aa551e41cb448bf4fd
Signeemmanuel <signeemmanuel28@gmail.com>
2022-12-10 18:49:00 -0800

    Update 4-print_alphabet.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,15 +1,20 @@
-#include <stdio.h>                                                                                                                                       /*
- *main: entry point of the program
- *return: 0
- */ 
-int main(void)                                                                                                                                           
-{                                                                                                                                                        
-	char ch;                                                                                                                                         
-	for (ch = 'a'; ch <= 'z'; ch++)                                                                                                                  
-		if(ch == 'e' || ch == 'q')
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always return 0 after successful execution of program
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'q' || ch == 'e')
 			continue;
-		putchar(ch);                                                                                                                             
-	putchar('\n');                                                                                                                                   
-	return (0);                                                                                                                                      
-}                                                                                                                                                        
-                                                                                                                                                         
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
