4dbc49fd07c4015a92fb98190e2d9c63b5e21850
Signeemmanuel <signeemmanuel28@gmail.com>
2022-12-10 18:07:39 -0800

    Upadate 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-/*
- *main: entry point of the program
- *reurn: 0
- * */
-
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always return 0 after successful execution of program
+ */
 int main(void)
 {
 	char ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 	putchar('\n');
 	return (0);
 }
-
