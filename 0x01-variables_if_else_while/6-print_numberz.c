7a6792b2ea312aaf84a42637fcd569533d0540d7
Signeemmanuel <signeemmanuel28@gmail.com>
2022-12-10 19:14:03 -0800

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
-/*
- *main: entry point of the progam
- *return: 0
+
+/**
+ * main - Entry point of the progam
+ *
+ * Return: 0
  */
 int main(void)
 {
 	char ch;
-	for(ch = '0'; ch <= '9'; ch++)
+
+	for (ch = '0'; ch <= '9'; ch++)
 		putchar(ch);
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
 
