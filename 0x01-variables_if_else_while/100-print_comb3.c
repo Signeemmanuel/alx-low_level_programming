1974c66a62db959cda474697dfa2a92394f7fdc2
Signeemmanuel <signeemmanuel28@gmail.com>
2022-12-11 18:58:15 -0800

    Upadate 100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,15 +8,15 @@
 
 int main(void)
 {
-	char ch1, ch2;
+	int ch1, ch2;
 
-	for (ch1 = '0'; ch1 <= '8'; ch1++)
+	for (ch1 = 0; ch1 <= 8; ch1++)
 	{
-		for (ch2 = ch1 + 1; ch2 <= '9'; ch2++)
+		for (ch2 = ch1 + 1; ch2 <= 9; ch2++)
 		{
-			putchar(ch1);
-			putchar(ch2);
-			if (!(ch1 == '8' && ch2 == '9'))
+			putchar(ch1 + '0');
+			putchar(ch2 + '0');
+			if (!(ch1 == 8 && ch2 == 9))
 			{
 				putchar(',');
 				putchar(' ');
