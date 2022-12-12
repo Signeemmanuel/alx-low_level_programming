469e34398f6df4d59030c223c36a3ea481c062b3
Signeemmanuel <signeemmanuel28@gmail.com>
2022-12-11 20:43:32 -0800

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,27 +9,33 @@
 int main(void)
 {
 	int ch1, ch2, ch3, ch4;
+	int tmp1 = 0;
+	int tmp2 = -1;
+	int cnt, num2;
 
 	for (ch1 = 0; ch1 <= 9; ch1++)
 	{
-		for (ch2 = 0; ch2 <= 9; ch2++)
+		for (ch2 = 0; ch2 <= 8; ch2++)
 		{
-			for (ch3 = 0; ch3 <= 9; ch3++)
+			cnt = 0;
+			for (ch3 = ch1; ch3 <= 9; ch3++)
 			{
-				for (ch4 = 0; ch4 <= 9; ch4++)
+				if (cnt == 0)
+					num2 = ch2 + 1;
+				else
+					num2 = 0;
+				cnt++;
+				for (ch4 = num2; ch4 <= 9; ch4++)
 				{
-					if (!(ch1 == 0 && ch2 == 0 && ch3 == 0 && ch4 == 0))
-					{
 					putchar(ch1 + '0');
 					putchar(ch2 + '0');
 					putchar(' ');
 					putchar(ch3 + '0');
 					putchar(ch4 + '0');
-					if (!(ch1 == 7 && ch2 == 8 && ch3 == 9))
+					if (!(ch1 == 9 && ch2 == 8 && ch3 == 9 && ch4 == 9))
 					{
 						putchar(',');
 						putchar(' ');
-					}
 					}
 				}
 			}
