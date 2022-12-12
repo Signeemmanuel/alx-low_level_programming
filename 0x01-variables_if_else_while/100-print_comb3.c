6e2a1c205dd7c670426e3c76f40eb9ad05913965
Signeemmanuel <signeemmanuel28@gmail.com>
2022-12-11 18:47:26 -0800

    Update 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entrry point into the program
+ *
+ * Return - Always 0
+ */
+
+int main(void)
+{
+	char ch1, ch2;
+
+	for (ch1 = '0'; ch1 <= '8'; ch1++)
+	{
+		for (ch2 = ch1 + 1; ch2 <= '9'; ch2++)
+		{
+			putchar(ch1);
+			putchar(ch2);
+			if (!(ch1 == '8' && ch2 == '9'))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
