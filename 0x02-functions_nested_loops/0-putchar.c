ebf7e05954b800aec886d2e61cdd9bf2ab46d93b
Signeemmanuel <signeemmanuel28@gmail.com>
2022-12-13 07:50:22 -0800

    Add 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point into the program
+ *
+ * Return: always 0 after succesfull execution of the program
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
