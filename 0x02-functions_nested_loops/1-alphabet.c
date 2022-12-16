699f4a2960347ec20d6c7f12250488f7835cbb84
Signeemmanuel <signeemmanuel28@gmail.com>
2022-12-16 09:34:38 -0800

    Add 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point into the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n')
+	return (0);
+}
