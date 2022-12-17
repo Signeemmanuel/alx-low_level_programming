d7b5099cabdc213b565e0bf5240560e18b36e9c6
Signeemmanuel <signeemmanuel28@gmail.com>
2022-12-16 21:21:29 -0800

    Add 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day starting from 00:00 to 23:59
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 5; k++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+			if (i == 2 && j == 3)
+			{
+				break;
+			}
+		}
+	}
+}
