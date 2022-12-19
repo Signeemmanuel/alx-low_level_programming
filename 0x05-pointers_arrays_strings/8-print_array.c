84e5334ecbdffc730fb3ecfa378cc16b6f42903f
Signeemmanuel <signeemmanuel28@gmail.com>
2022-12-19 09:26:08 -0800

    Add 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_array - Prints n elements of an array of integers
+ * @a: An array.
+ * @n: Size of array.
+ *
+ * Return: void.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (a[i] < 0)
+		{
+			_putchar('-');
+			a[i] = -a[i];
+		}
+		if (a[i] / 10000 >= 1)
+			_putchar(((a[i] / 10000) % 10) + '0');
+		if (a[i] / 1000 >= 1)
+			_putchar(((a[i] / 1000) % 10) + '0');
+		if (a[i] / 100 >= 1)
+			_putchar(((a[i] / 100) % 10) + '0');
+		if (a[i] / 10 >= 1)
+			_putchar(((a[i] / 10) % 10) + '0');
+		_putchar((a[i] % 10) + '0');
+		if (i != n)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
