1fd62d5f288422ec87a4d2530328b8cf6008bdb1
Signeemmanuel <signeemmanuel28@gmail.com>
2022-12-21 18:45:15 -0800

    Add 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.c"
+
+/**
+ * _strcat - this fuction will concatenates two strings
+ * @dest: the string that will modified.
+ * @src: the string that ill be concatenated to @des.
+ *
+ * Return: a pointer to @dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int count = 0;
+	int size_dest;
+
+	while (dest[count] != '\0')
+		count++;
+	size_dest = count;
+	for (count = 0; src[count]; count++)
+	{
+		dest[size_dest++] = src[count];
+	}
+	return (dest);
+}
