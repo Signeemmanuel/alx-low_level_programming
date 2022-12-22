e4352306298ab384141c952472797241ee69f141
Signeemmanuel <signeemmanuel28@gmail.com>
2022-12-21 19:00:18 -0800

    Add 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.c"
+
+/**
+ * _strncat - this fuction will concatenates two strings
+ * @dest: the string that will modified.
+ * @src: the string that ill be concatenated to @des.
+ * @n: concatenate up to position n.
+ * Return: a pointer to @dest
+ */
+char *_strcat(char *dest, char *src, int n)
+{
+	int count = 0;
+	int size_dest;
+
+	while (dest[count] != '\0')
+		count++;
+	size_dest = count;
+	for (count = 0; src[count] && count < n; count++)
+	{
+		dest[size_dest++] = src[count];
+	}
+	return (dest);
+}
