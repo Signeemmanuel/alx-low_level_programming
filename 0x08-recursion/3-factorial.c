1e5281b0bfd02b65b51ddf6283d592874565609f
Signeemmanuel <signeemmanuel28@gmail.com>
2023-02-06 01:57:20 -0800

    fix the return value in the factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -11,7 +11,7 @@ int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (n <= 1)
+	if (n == 0 || n == 1)
 		return (1);
-	return (factorial(n - 1) + factorial(n - 2));
+	return (n * factorial(n - 1));
 }
