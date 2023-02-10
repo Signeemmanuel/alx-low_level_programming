03ab116595e9a344425a5819a34ef1050061bb64
Signeemmanuel <signeemmanuel28@gmail.com>
2023-02-09 18:45:25 -0800

    Add 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point into the program.
+ * @argc: number of command line arguments.
+ * @argv: array of strings of command line arguments.
+ *
+ * Return: 0 (Success).
+ */
+int main(int argc, char *argv[])
+{
+	char *name = argv[0];
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
