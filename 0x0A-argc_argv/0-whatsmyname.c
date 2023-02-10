b5dc5f82f5b304a80026962c2d657962219a2242
Signeemmanuel <signeemmanuel28@gmail.com>
2023-02-09 18:16:06 -0800

    Add 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point into the program.
+ * @argc: number of command line arguments.
+ * @argv: array of strings of command line arguments.
+ *
+ * Return: 0 (Success).
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (argv[0][i] != 0)
+	{
+		_putchar(argv[0][i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
